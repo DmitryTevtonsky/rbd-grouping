4330c018f7e18ea9e1cb956e2bb017baced59467
tevtonsky1054m1@gmail.com <tevtonsky1054m1@gmail.com>
2020-03-09 23:15:03 +0300

    +Fixes.

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { DragDropContext } from 'react-beautiful-dnd';
+import { getInitialData } from './data';
+import Column from './components/Column';
 
 const App = () => {
+  const [data, setData] = useState(getInitialData());
+  console.log('DATA', data);
+  const onDragEnd = result => {
+    console.log('result', result);
+    const { draggableId, destination, source } = result;
+    if (!destination) return;
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = data.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = { ...column, taskIds: newTaskIds };
+
+    const newData = {
+      ...data,
+      columns: {
+        ...data.columns,
+        [newColumn.id]: newColumn
+      }
+    };
+    console.log('newData', newData);
+    setData(newData);
+  };
+
   return (
     <div className="App">
-
+      <DragDropContext onDragEnd={onDragEnd}>
+        {data.columnsOrder.map(columnId => {
+          const column = data.columns[columnId];
+          const tasks = column.taskIds.map(taskId => data.tasks[taskId]);
+          return (
+            <Column key={columnId} column={column} tasks={tasks}>
+              {column.title}
+            </Column>
+          );
+        })}
+      </DragDropContext>
     </div>
   );
-}
+};
 
 export default App;
--- src/app/data.js
+++ src/app/data.js
@@ -1,11 +1,30 @@
-export const setData = () => {
-    const data = {}
-    for (let index = 0; index < 10; index++) {
-        const item = {
-            id: `task${index}}`,
-            content: 'lol'
-        }
-        Object.assign(data, item)
-    }
-    return data
-}
+import faker from 'faker';
+
+export const getInitialData = (count: 5) => {
+  const tasks = {};
+  const columns = {};
+  for (let index = 0; index < 5; index++) {
+    const taskItem = {
+      id: `task-${index}`,
+      content: faker.random.word()
+    };
+    tasks[`task-${index}`] = taskItem;
+  }
+  const taskIds = Object.keys(tasks);
+
+  for (let index = 0; index < 1; index++) {
+    const columnItem = {
+      id: `column-${index}`,
+      title: `Column-${index}`,
+      taskIds
+    };
+    columns[`column-${index}`] = columnItem;
+  }
+  const columnsOrder = Object.keys(columns);
+
+  return {
+    tasks,
+    columns,
+    columnsOrder
+  };
+};
