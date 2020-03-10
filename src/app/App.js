04b1b80d98c28afd66adfb28e21c99151ef4fc80
Dmitry Prusakov <prusakov@sedmax.com>
2020-03-10 10:17:03 +0300

    Grouping ready.

--- src/app/App.js
+++ src/app/App.js
@@ -7,6 +7,7 @@ import Column from './components/Column';
 const App = () => {
   const [data, setData] = useState(getInitialData());
   console.log('DATA', data);
+
   const onDragEnd = result => {
     console.log('result', result);
     const { draggableId, destination, source } = result;
@@ -19,36 +20,63 @@ const App = () => {
       return;
     }
 
-    const column = data.columns[source.droppableId];
-    const newTaskIds = Array.from(column.taskIds);
-    newTaskIds.splice(source.index, 1);
-    newTaskIds.splice(destination.index, 0, draggableId);
+    const startColumn = data.columns[source.droppableId];
+    const finishColumn = data.columns[destination.droppableId];
 
-    const newColumn = { ...column, taskIds: newTaskIds };
+    if (startColumn === finishColumn) {
+      const newTaskIds = Array.from(startColumn.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
 
-    const newData = {
-      ...data,
-      columns: {
-        ...data.columns,
-        [newColumn.id]: newColumn
-      }
-    };
-    console.log('newData', newData);
-    setData(newData);
+      const newColumn = { ...startColumn, taskIds: newTaskIds };
+
+      const newData = {
+        ...data,
+        columns: {
+          ...data.columns,
+          [newColumn.id]: newColumn
+        }
+      };
+
+      setData(newData);
+    } else {
+      const startTaskIds = Array.from(startColumn.taskIds);
+      startTaskIds.splice(source.index, 1);
+      const newStartColumn = { ...startColumn, taskIds: startTaskIds };
+      console.log('newStartColumn', newStartColumn);
+
+      const finishTaskIds = Array.from(finishColumn.taskIds);
+      finishTaskIds.splice(destination.index, 0, draggableId);
+      const newFinishColumn = { ...finishColumn, taskIds: finishTaskIds };
+      console.log('newFinishColumn', newFinishColumn);
+
+      const newData = {
+        ...data,
+        columns: {
+          ...data.columns,
+          [newStartColumn.id]: newStartColumn,
+          [newFinishColumn.id]: newFinishColumn
+        }
+      };
+      console.log('newData', newData);
+      setData(newData);
+    }
   };
 
   return (
     <div className="App">
       <DragDropContext onDragEnd={onDragEnd}>
-        {data.columnsOrder.map(columnId => {
-          const column = data.columns[columnId];
-          const tasks = column.taskIds.map(taskId => data.tasks[taskId]);
-          return (
-            <Column key={columnId} column={column} tasks={tasks}>
-              {column.title}
-            </Column>
-          );
-        })}
+        <div className="container">
+          {data.columnsOrder.map(columnId => {
+            const column = data.columns[columnId];
+            const tasks = column.taskIds.map(taskId => data.tasks[taskId]);
+            return (
+              <Column key={columnId} column={column} tasks={tasks}>
+                {column.title}
+              </Column>
+            );
+          })}
+        </div>
       </DragDropContext>
     </div>
   );
--- src/app/data.js
+++ src/app/data.js
@@ -1,22 +1,24 @@
 import faker from 'faker';
 
-export const getInitialData = (count: 5) => {
+export const getInitialData = (count: 3) => {
   const tasks = {};
   const columns = {};
-  for (let index = 0; index < 5; index++) {
+  for (let index = 0; index < 4; index++) {
     const taskItem = {
       id: `task-${index}`,
       content: faker.random.word()
     };
     tasks[`task-${index}`] = taskItem;
   }
-  const taskIds = Object.keys(tasks);
 
-  for (let index = 0; index < 1; index++) {
+  for (let index = 0; index < 3; index++) {
+    // const taskIds = Object.keys(tasks);
+    const taskIdsForThisColumn = Object.keys(tasks).splice(index * 3, 3);
+    // console.log(taskIdsForThisColumn);
     const columnItem = {
       id: `column-${index}`,
       title: `Column-${index}`,
-      taskIds
+      taskIds: taskIdsForThisColumn
     };
     columns[`column-${index}`] = columnItem;
   }
